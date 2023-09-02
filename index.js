2d93513d3e17866be233557f33fbff2663277b29
abu bakar <abubakarseddek1999@gmail.com>
2023-09-02 14:59:22 +0600

    short add

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ const handleItem = async (categoryId) => {
 
     const itemCard = document.getElementById('item-card')
     itemCard.innerHTML = '';
-    data.data?.sort((a,b)=> parseInt(b.others.views) -parseInt(a.others.views)).forEach(item => {
+    data.data?.forEach(item => {
         // console.log(item)
         const postedDate =item.others?.posted_date
         // console.log(postedDate)
@@ -109,6 +109,93 @@ const handleItem = async (categoryId) => {
 
 
 
+}
+
+const shortHandle =async(categoryId)=>{
+     const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+    const data = await res.json();
+    console.log(data);
+
+    const emptyArea =document.getElementById('no-data')
+    console.log(data.data);
+    
+    if (data.data.length ===0) {
+        emptyArea.classList.remove('hidden');
+        
+            
+    }
+    else{
+        emptyArea.classList.add('hidden');
+    }
+
+
+
+    const itemCard = document.getElementById('item-card')
+    itemCard.innerHTML = '';
+    data.data?.sort((a,b)=> parseInt(b.others.views) -parseInt(a.others.views)).forEach(item => {
+        // console.log(item)
+        const postedDate =item.others?.posted_date
+        // console.log(postedDate)
+        
+        const toHoursAndMinute = ()=>{
+            
+            const totalMinute =Math.floor(postedDate /60);
+            const second = postedDate %60;
+            const hours =Math.floor(totalMinute /60);
+            const minute= totalMinute %60;
+            
+            const result ={h: hours , m: minute, s: second}
+            // console.log(result)
+            return result
+            
+        }
+        toHoursAndMinute()
+        
+        const time =toHoursAndMinute()
+        var setTime = `${time.h}hrs ${time.m}min ago `
+
+        // console.log(time)
+       
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card mt-4  bg-base-100 shadow-xl">
+            <figure ><img class=" w-full h-56" src=${item.thumbnail
+            } alt="Shoes" />
+            </figure>
+            <div class="flex  justify-end">
+            <div class=" -mt-8 bg-black text-white ">${postedDate? setTime : ""}</div>
+            </div>
+            <div class="card-body ">
+            
+            <h2 class="card-title">
+               <div class=" rounded-md">
+                <img class="w-14 h-14 rounded-full" src="${item.authors[0]?.profile_picture}" alt=""/>
+               </div>
+                <span class=" text-xl font-bold">${item.title} </span>
+            </h2>
+            <h2 class="card-title">
+               <div class="text-xl ">
+                    <p>${item.authors[0].profile_name
+                    }</p>
+               </div>
+              
+               <div>${item.authors[0].verified ? '<img src="tick.png"/>' :  " "}
+               </div>
+                
+            </h2>
+            
+            
+            <p>${item.others?.views
+            } views</p>
+            
+            </div>
+        </div>
+        `
+        itemCard.appendChild(div);
+     
+
+    });
+   
 }
 
 handleItem(1000);
